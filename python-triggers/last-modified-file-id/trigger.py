5b2fdd117080473aac62cf3742f33714c748db5b
Alex Bourret <alexandre.bourret@dataiku.com>
2025-08-28 22:21:14 +0200

    replacing get_info with variable for storing last dataset update

--- python-triggers/last-modified-file-id/trigger.py
+++ python-triggers/last-modified-file-id/trigger.py
@@ -3,8 +3,9 @@ from dataiku.customtrigger import get_plugin_config
 from dataiku.customtrigger import get_trigger_config
 from dataiku.scenario import Trigger
 from dku_googledrive.session import GoogleDriveSession
-from dataiku import Dataset, default_project_key
+from dataiku import Dataset, default_project_key, Project
 from datetime import datetime
+import time
 
 
 logger = logging.getLogger(__name__)
@@ -18,6 +19,10 @@ trigger_config = get_trigger_config()
 trigger = Trigger()
 config = plugin_config.get("config", {})
 
+project = Project()
+variables = project.get_variables()
+prefix = config.get("prefix", "googlesheets_trigger_")
+
 google_sheet_file_id = config.get("google_sheets_file_id")
 if google_sheet_file_id is None:
     logger.error("File ID is empty")
@@ -28,6 +33,10 @@ if target_dataset is None:
     logger.error("Target dataset is empty")
     raise Exception("Target dataset cannot be left empty")
 
+key_name = "{}{}".format(prefix, target_dataset)
+key_value = variables.get("standard", {}).get(key_name, 0)
+local_dataset_last_modified = key_value
+
 plugin_config = plugin_config.get("pluginConfig", {})
 session = GoogleDriveSession(config, plugin_config)
 
@@ -36,17 +45,24 @@ remote_file_last_modified_epoch = 0
 try:
     remote_file_last_modified_epoch = int(datetime.strptime(remote_file_last_modified, "%Y-%m-%dT%H:%M:%S.%f%z").timestamp()) * 1000
 except Exception as error_message:
-    logger.error("Could not convert remote file last modified date: {}. Error {}".format(remote_file_last_modified,error_message ))
+    logger.error("Could not convert remote file last modified date: {}. Error {}".format(
+            remote_file_last_modified,
+            error_message
+        )
+    )
 
-local_dataset_info = Dataset(target_dataset, project_key=current_project_key, ignore_flow=False).get_files_info()
-local_dataset_global_paths =local_dataset_info.get("globalPaths", [])
-local_dataset_last_modified = None
-if len(local_dataset_global_paths) > 0:
-    local_dataset_last_modified = local_dataset_global_paths[0].get("lastModified")
-
+local_dataset_info = Dataset(
+    target_dataset,
+    project_key=current_project_key,
+    ignore_flow=False
+).get_files_info()
+local_dataset_global_paths = local_dataset_info.get("globalPaths", [])
 if isinstance(remote_file_last_modified_epoch, int) and isinstance(local_dataset_last_modified, int):
     if remote_file_last_modified_epoch > local_dataset_last_modified:
         logger.info("remote epoch {} > local epoch {}, firing the trigger".format(remote_file_last_modified_epoch, local_dataset_last_modified))
+        remote_file_last_modified_epoch = int(time.time()) * 1000
+        variables["standard"][key_name] = remote_file_last_modified_epoch
+        project.set_variables(variables)
         trigger.fire()
     else:
         logger.info("Target dataset is up to date")
