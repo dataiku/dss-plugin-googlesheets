a1fe3c2951f9a8ff9d0a0d5a1e4b562cbd55f5c6
Alex Bourret <alexandre.bourret@dataiku.com>
2025-09-18 11:11:19 +0200

    add multisheets append recipe

---
+++ custom-recipes/googlesheets-multisheets/recipe.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+import datetime
+import dataiku
+from dataiku.customrecipe import get_output_names_for_role, get_recipe_config
+from googlesheets import GoogleSheetsSession
+from safe_logger import SafeLogger
+from googlesheets_common import DSSConstants, extract_credentials, assert_not_forbidden_dataset_type
+from time import sleep
+from googlesheets_append import append_rows
+
+
+logger = SafeLogger("googlesheets plugin", ["credentials", "access_token"])
+
+logger.info("GoogleSheets multisheets v{} starting".format(DSSConstants.PLUGIN_VERSION))
+
+output_name = get_output_names_for_role('output_role')[0]
+output_dataset = dataiku.Dataset(output_name)
+config = get_recipe_config()
+
+logger.info("config parameters: {}".format(logger.filter_secrets(config)))
+
+doc_id = config.get("doc_id")
+if not doc_id:
+    raise ValueError("The document id is not provided")
+
+credentials, credentials_type = extract_credentials(config)
+session = GoogleSheetsSession(credentials, credentials_type)
+insert_format = config.get("insert_format")
+write_mode = config.get("write_mode", "append")
+batch_size = config.get("batch_size", 200)
+insertion_delay = config.get("insertion_delay", 0)
+
+tabs_mapping = config.get("tabs_mapping", [])
+
+for tab_mapping in tabs_mapping:
+    input_name = tab_mapping.get("input_dataset")
+    input_dataset = dataiku.Dataset(input_name)
+    input_schema = input_dataset.read_schema()
+    tab_id = tab_mapping.get("output_sheet_name")
+    if not tab_id:
+        raise ValueError("The sheet name is not provided")
+
+    # This recipe principle is often misunderstood and many users output it to the dataset that they mean to append to
+    # We check that this is not the case here and if so fail with error message pointing to the doc
+    assert_not_forbidden_dataset_type(output_dataset, "CustomPython_googlesheets-sheet", doc_id, "Google Sheet")
+    output_dataset.write_schema(input_schema)
+
+    # Load worksheet
+    worksheet = session.get_spreadsheet(doc_id, tab_id)
+
+    worksheet.append_rows = append_rows.__get__(worksheet, worksheet.__class__)
+
+    # Handle datetimes serialization
+    def serializer_iso(obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+        return obj
+
+    def serializer_dss(obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.strftime(DSSConstants.GSPREAD_DATE_FORMAT)
+        return obj
+
+    if insert_format == "USER_ENTERED":
+        serializer = serializer_dss
+    else:
+        serializer = serializer_iso
+
+    # Open writer
+    writer = output_dataset.get_writer()
+
+    # Iteration row by row
+    batch = []
+    if write_mode == "overwrite":
+        worksheet.clear()
+        columns = [column["name"] for column in input_schema]
+        batch.append(columns)
+    for row in input_dataset.iter_rows():
+
+        # write to spreadsheet by batch
+        batch.append([serializer(v) for k, v in list(row.items())])
+
+        if len(batch) >= batch_size:
+            if insertion_delay > 0:
+                sleep(0.01 * insertion_delay)
+            worksheet.append_rows(batch, insert_format)
+            batch = []
+
+        # write to output dataset
+        writer.write_row_dict(row)
+
+    if len(batch) > 0:
+        worksheet.append_rows(batch, insert_format)
+
+    # Close writer
+    writer.close()
