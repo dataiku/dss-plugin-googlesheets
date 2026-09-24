6754ed35ef5802a1394aa3704860fe4086195542
Alex Bourret <alexandre.bourret@dataiku.com>
2026-09-24 16:36:54 +0200

    replace eval with json.loads

--- python-lib/dku_googledrive/session.py
+++ python-lib/dku_googledrive/session.py
@@ -48,7 +48,9 @@ class GoogleDriveSession():
             credentials = AccessTokenCredentials(self.access_token, "dss-googledrive-plugin/2.0")
             http_auth = credentials.authorize(Http())
         else:
-            credentials_dict = eval(config.get("preset_credentials_service_account", {}).get("credentials", ""))
+            credentials_dict = json.loads(config.get("preset_credentials_service_account", {}).get("credentials", ""))
+            if not isinstance(credentials_dict, dict):
+                raise GoogleDriveSessionError("The credential is not in the correct format")
             credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scopes)
             http_auth = credentials.authorize(Http())
         self.root_id = config.get("googledrive_root_id")
